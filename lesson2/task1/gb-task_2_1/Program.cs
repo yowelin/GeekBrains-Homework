c4218d915bde310a540dcd7649423a7a18cdfed4
yowelin89 <semikin.in@gmail.com>
2021-02-20 20:10:59 +0300

    Tasks 1 and 2 from lesson 2 done

---
+++ lesson2/task1/gb-task_2_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace gb_task_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double minTemp = 0;
+            double maxTemp = 0;
+            bool isMinTempNumber = false;
+            bool isMaxTempNumber = false;
+            beginning:
+            do
+            {
+                isMinTempNumber = false;
+                Console.WriteLine("Введите минимальную температуру за сутки:");
+                isMinTempNumber = Double.TryParse(Console.ReadLine(), out minTemp);
+                if (isMinTempNumber)
+                {
+                    do
+                    {
+                        isMaxTempNumber = false;
+                        Console.WriteLine("Введите максимальную температуру за сутки:");
+                        isMaxTempNumber = Double.TryParse(Console.ReadLine(), out maxTemp);
+                        if (isMaxTempNumber)
+                        {
+                            if (minTemp <= maxTemp)
+                            {
+                                Console.WriteLine($"Среднесуточная температура: {(minTemp + maxTemp) / 2}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Минимальная температура больше максимальной. Повторите ввод.");
+                                goto beginning;
+                            }
+                        }
+                    } while (!isMaxTempNumber);
+                }
+            } while (!isMinTempNumber);
+        }
+    }
+}
