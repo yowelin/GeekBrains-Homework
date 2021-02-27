e849bab9258482ea8f4b6de160d4ca6f6a2027b2
Ivan <semikin.in@gmail.com>
2021-02-27 09:30:07 +0300

    task 2 is begun

---
+++ lesson3/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        string[,] telBook = new string[2,0];
+        static void Main(string[] args)
+        {
+            if (args[0] == "-add")
+            {
+                AddContact();
+            }
+        }
+
+        static void AddContact()
+        {
+
+        }
+    }
+}
