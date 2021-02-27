7d6c09162f1cc2ca2c7b37bec15a38b9e6cff104
yowelin89 <semikin.in@gmail.com>
2021-02-27 07:37:58 +0300

    task 2 done

--- lesson3/task2/Program.cs
+++ lesson3/task2/Program.cs
@@ -4,18 +4,41 @@ namespace task2
 {
     class Program
     {
-        string[,] telBook = new string[2,0];
+        static string[,] telBook = new string[0, 2];
         static void Main(string[] args)
         {
-            if (args[0] == "-add")
-            {
-                AddContact();
-            }
+            do {
+                telBook = AddContact();
+                showContacts();
+            } while (Console.ReadLine().ToString().ToLower() != "exit");
         }
 
-        static void AddContact()
+        static string[,] AddContact()
         {
+            string contactName = "";
+            string contactTelOrEmail = "";
+            Console.Write("Введите имя контакта: ");
+            contactName = Console.ReadLine();
+            Console.Write("Введите телефон или e-mail контакта: ");
+            contactTelOrEmail = Console.ReadLine();
+            string[,] newTelBook = new string[telBook.GetLength(0) + 1, 2];
+            for (int i = 0; i < telBook.GetLength(0); i++)
+            {
+                newTelBook[i, 0] = telBook[i, 0];
+                newTelBook[i, 1] = telBook[i, 1];
+            }
+            newTelBook[telBook.GetLength(0), 0] = contactName;
+            newTelBook[telBook.GetLength(0), 1] = contactTelOrEmail;
 
+            return newTelBook;
+        }
+
+        static void showContacts()
+        {
+            for (int i = 0; i < telBook.GetLength(0); i++)
+            {
+                Console.WriteLine("Contact {0}: {1,-25} {2,15}", i+1, telBook[i, 0], telBook[i, 1]);
+            }
         }
     }
 }
