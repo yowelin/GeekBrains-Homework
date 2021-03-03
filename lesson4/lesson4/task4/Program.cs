e390bca173595dd174251606dff05d7f75b35dd2
Ivan <semikin.in@gmail.com>
2021-03-03 16:11:49 +0300

    task 4 done

---
+++ lesson4/lesson4/task4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            beginning:
+            UInt64 fibonacciNumber = 0;
+            Console.WriteLine("Сколько чисел Фибоначчи вывести на экран?");
+            if (UInt64.TryParse(Console.ReadLine(), out fibonacciNumber) & fibonacciNumber > 0)
+            {
+                for (UInt64 i = 0; i < fibonacciNumber; i++)
+                {
+                    Console.Write(GetFibonacci(i) + ( i == fibonacciNumber - 1 ? ", ...": ", "));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введите натуральное число.");
+                goto beginning; // лень писать цикл
+            }
+        }
+        static UInt64 GetFibonacci(UInt64 number)
+        {
+            if (number == 1 | number == 0) // терминальное условие
+            {
+                return number;
+            }
+            return GetFibonacci(number - 1) + GetFibonacci(number - 2); // рекурсивный вызов
+        }
+    }
+}
