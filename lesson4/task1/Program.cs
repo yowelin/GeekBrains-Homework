fde481fc16319bc43decfaff0af3726a7ae147eb
yowelin89 <semikin.in@gmail.com>
2021-03-06 11:37:24 +0300

    All Tasks from lesson 4 done

---
+++ lesson4/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task1
+{
+    class FIO 
+    {
+        public string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return firstName + " " + lastName + " " + patronymic;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FIO fio = new FIO();
+            Console.WriteLine(fio.GetFullName("Семикин", "Иван", "Николаевич"));
+            Console.WriteLine(fio.GetFullName("Трофимова", "Татьяна", "Кимовна"));
+            Console.WriteLine(fio.GetFullName("Новикова", "Ольга", "Викторовна"));
+        }
+    }
+}
