456daabd29410cce63baab8a30f5c7d48d2b80a6
yowelin89 <semikin.in@gmail.com>
2021-03-06 13:46:52 +0300

    All tasks from lesson4 done

---
+++ lesson4/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task2
+{
+    class Counter
+    {
+        public int CountSumOfNumbers(string numbers)
+        {
+            string[] splittedString = numbers.Split(" ");
+            int[] splittedNumbers = new int[splittedString.GetLength(0)];
+            int sum = 0;
+            for (int i = 0; i < splittedString.GetLength(0); i++)
+            {
+                if(Int32.TryParse(splittedString[i], out splittedNumbers[i]))
+                {
+                    sum += splittedNumbers[i];
+                }
+            }
+            return sum;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter cntr = new Counter();
+            Console.Write("Введите числа через пробел: ");
+            Console.Write("Сумма введеных чисел: " + cntr.CountSumOfNumbers(Console.ReadLine()));
+        }
+    }
+}
---
+++ lesson4/task3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace task3
+{
+    enum Season
+    {
+        Winter = 1,
+        Spring = 2,
+        Summer = 3,
+        Autumn = 4
+    }
+    class SeasonGetter
+    {
+        public Season GetSeason(int month)
+        {
+            if (month == 12 | month == 1 | month == 2)
+            {
+                return Season.Winter;
+            }
+            if (month == 3 | month == 4 | month == 5)
+            {
+                return Season.Spring;
+            }
+            if (month == 6 | month == 7 | month == 8)
+            {
+                return Season.Summer;
+            }
+            if (month == 9 | month == 10 | month == 11)
+            {
+                return Season.Autumn;
+            }
+            return 0;
+        }
+        public string ConvertSeasonToString(Season season)
+        {
+            switch (season)
+            {
+                case Season.Winter:
+                    return "Зима";
+                case Season.Spring:
+                    return "Весна";
+                case Season.Summer:
+                    return "Лето";
+                case Season.Autumn:
+                    return "Осень";
+            }
+            return "Ошибка: введите число от 1 до 12";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SeasonGetter sg = new SeasonGetter();
+            int seasonNum = 0;
+            Console.WriteLine("Введите число месяца: ");
+            if (Int32.TryParse(Console.ReadLine(), out seasonNum))
+            {
+                Console.WriteLine(sg.ConvertSeasonToString(sg.GetSeason(seasonNum)));
+            }
+
+        }
+    }
+}
